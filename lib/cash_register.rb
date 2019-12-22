82beb3292cf82b4c53f166f46f49239c8c64faa9
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-22 16:12:43 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -25,6 +25,16 @@ class CashRegister
     end
   end
   
+  def apply_discount()
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
+  
   
   
 end
