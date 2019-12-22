364fedcb75b990421acc4f9bd076cb08239ce9e5
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-22 16:09:22 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -15,4 +15,12 @@ class CashRegister
   def add_item_2(cost, name, number = 1)
     self.total = cost * number
 
-  def apply_discount
+  def apply_discount()
+    if @discount > 0
+      @discount = @discount/100.to_f
+      @total = @total - (@total * (@discount))
+      "After the discount, the total comes to $#{@total.to_i}."
+    else
+      "There is no discount to apply."
+    end
+  end
