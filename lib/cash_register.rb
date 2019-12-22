2d0add8213128396066ddf25c578b605ea12fbad
Sehrish Balouch <sehrishbalouch4@gmail.com>
2019-12-22 16:14:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -35,6 +35,21 @@ class CashRegister
     end
   end
   
+  def add_item(title,price,quantity = 1)
+    if quantity>1
+      i=0
+      while i<quantity
+        @items << title
+        i+=1
+      end
+    else
+      @items << title
+    end
+    @total += price*quantity
+    @last_transaction_amount = @total
+    @total
+  end
+  
   
   
 end
